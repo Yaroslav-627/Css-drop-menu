035e368fb146419bd3e4d701e69047e3218d8343
Yaroslav-627 <yarslavpopovych@gmail.com>
2022-09-30 19:39:35 +0300

    first time

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// let li = document.querySelector("li");
+// li.addEventListener("click", () =>{
+//     // li.toggleAttribute("style:active;");
+//     li.addClass("active")
+//     console.log(li);
+// });
